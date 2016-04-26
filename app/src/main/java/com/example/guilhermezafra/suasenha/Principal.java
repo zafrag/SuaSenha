c4861e673ddf5fab1d71bfdf1d9d6d2a90be9911
zafrag <guilherme_zafra@hotmail.com>
2016-04-25 21:50:23 -0300

    Initial commit

---
+++ app/src/main/java/com/example/guilhermezafra/suasenha/Principal.java
@@ -0,0 +1,57 @@
+package com.example.guilhermezafra.suasenha;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+
+
+
+
+public class Principal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Intent testar = getIntent();
+        String teste = testar.getExtras().getString("suasenha");
+        TextView suasenha = (TextView) findViewById(R.id.idSenha);
+        suasenha.setText("Sua senha é: " + teste);
+
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+        int second = c.get(Calendar.SECOND);
+
+        final TextView horaincluso = (TextView) findViewById(R.id.hora);
+        horaincluso.setText("Horário Incluso: " + hour + ":" + minute + ":" + second);
+
+
+
+
+
+    }
+    }
+
+
